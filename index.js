92b7932a482bbe42e56d0a4a517849ca176e7c13
Chelsea <chelsea.icaza10@hotmail.com>
2021-07-23 22:37:23 -0400

    commit tree 3ad0f86666f4c315918838243fe526e87ad49aa8

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+// function findMatching(driveArr, names){
+//     for (const driver of driveArr){
+//        if(drivers[''] === names){
+//            return 
+//        }
+//     }
+
+const { stringContaining } = require("expect")
+
+// }
+
+function findMatching(drivers, nameInput){
+    const newDrivers = drivers.filter(name => name.toLowerCase() === nameInput.toLowerCase())
+    return  newDrivers
+
+   // console.log(newDrivers)
+}
+
+function fuzzyMatch(drivers, nameInput){
+    const newFuzzy = drivers.filter(name => name === nameInput.includes())
+    return newFuzzy
+
+}
+
+function matchName(drivers, callB){
+    const newMatch = []; 
+
+    for (const drInfo of drivers){
+        if (callB(drInfo)){
+            newMatch.push(user)
+        }
+    }
+}
